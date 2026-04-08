bc8e1c33a4546bef9862e9c011a735c5761d3177
m-ozbat-vccs <mno16480@email.vcc.edu>
2026-04-08 14:46:46 -0400

    cleaned up javascript, applied w3schools accordion approach

--- accordion-script.js
+++ accordion-script.js
@@ -1,36 +1,6 @@
 "use strict";
-// Select all elements with the class 'faq-link'
-const faqLinks = document.querySelectorAll(".faq-link");
 
-faqLinks.forEach((link) => {
-    link.addEventListener("click", function(event) {
-        event.preventDefault(); // Prevent the link from jumping the page
-
-        // Find the icon image related to the clicked link
-        // This assumes the icon is a sibling or child of the parent container
-        const icon = this.parentElement.querySelector(".faq-icon");
-        
-        // Define your icon paths
-        const plusIcon = "assets/images/icon-plus.svg";
-        const minusIcon = "assets/images/icon-minus.svg";
-
-        // Swap the image source based on current state
-        if (icon.getAttribute("src") === plusIcon) {
-            icon.setAttribute("src", minusIcon);
-        } else {
-            icon.setAttribute("src", plusIcon);
-        }
-
-        // Toggle answer
-        const answer = this.parentElement.querySelector(".question-block");
-        if (answer) {
-            answer.style.display = (answer.style.display === "none") ? "block" : "none";
-        }
-    });
-});
-
-//Reveal hidden paragraph because the top didn't do it...if top 'toggle answer' is removed, it breaks???
-var acc = document.getElementsByClassName("question-header");
+var acc = document.getElementsByClassName("faq-btn");
 var i;
 
 for (i = 0; i < acc.length; i++) {
